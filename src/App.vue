ab3999b405e9141e2cc982a7f20d5f87a10b7e54
Ricardo <ricardo.baldrez@gmail.com>
2020-10-05 14:41:17 -0300

    Utilização do método created + requisição HTTP na pokeapi com axios

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,25 @@
 
 <script>
   import axios from 'axios';
-  
+
   export default {
     name: 'App',
+    data() {
+      return {
+        pokemons: []
+      }
+    },
+    created: function() { // created é o método chamado assim que o componente for 'criado/chamado' dentro da aplicação(assim como no react temos o componentDidMount)
+      axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+        .then((response) => {
+          console.log("Requisição de pokemons feita com sucesso!!!");
+          this.pokemons = response.data.results;
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+        .finally(console.log('Finalizou'));
+    }
   }
 </script>
 
