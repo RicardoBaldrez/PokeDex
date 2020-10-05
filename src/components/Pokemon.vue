e14c8d163585a138f46563c034ae9b19012e160d
Ricardo <ricardo.baldrez@gmail.com>
2020-10-05 15:59:00 -0300

    Buscando mais informações dos pokemons

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -6,11 +6,30 @@
 </template>
 
 <script>
+	import axios from 'axios';
+
 	export default {
+		created: function() {
+			axios.get(this.url)
+				.then((response) => {
+					this.pokemon.type = response.data.types[0].type.name;
+					this.pokemon.imgFront = response.data.sprites.front_default;
+					this.pokemon.imgBack = response.data.sprites.back_default;
+
+					console.log(this.pokemon);
+				})
+		},
+		data() {
+			return {
+				pokemon: {
+
+				}
+			}
+		},
 		props: {
-			number: Number,
 			name: String,
-			url: String
+			url: String,
+			number: Number,
 		},
 		filters: {
 			upperFirstLetter: function(value) {
